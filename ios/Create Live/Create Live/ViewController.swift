d1d0a8786ea7611bf016252df1f3f798b4084ea9
Chris Tibbs <eggyeggbean@google.com>
2020-04-27 15:26:17 -0400

    Add blank Xcode project

---
+++ ios/Create Live/Create Live/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Create Live
+//
+//  Created by Chris Tibbs on 4/27/20.
+//  Copyright © 2020 FB Live. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
