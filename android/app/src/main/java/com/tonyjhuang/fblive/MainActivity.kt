27fbb867582f4b7af7feb49c2ffba5c339147156
Tony <tony.huang.jun@gmail.com>
2020-04-28 20:49:56 -0700

    add firebase, fragment scaffolding

--- android/app/src/main/java/com/tonyjhuang/fblive/MainActivity.kt
+++ android/app/src/main/java/com/tonyjhuang/fblive/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tonyjhuang.fblive
 
 import android.os.Bundle
+import android.view.Window
 import android.view.WindowManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,20 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        setUpNavBar(navView)
+        setContentView(R.layout.activity_main)//Remove title bar
         // BAD
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-    }
-
-    private fun setUpNavBar(navView: BottomNavigationView) {
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_watch_stream, R.id.navigation_broadcast, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
---
+++ android/app/src/main/java/com/tonyjhuang/fblive/ui/login/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.tonyjhuang.fblive.ui.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+
+import com.tonyjhuang.fblive.R
+
+class LoginFragment : Fragment() {
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findNavController().navigate(LoginFragmentDirections.actionLoginToHome())
+    }
+}
