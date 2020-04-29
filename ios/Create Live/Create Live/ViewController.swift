afd12ddb4f4749bbd93a80aeb8d11c582215598b
vywu <vivianyuwu@gmail.com>
2020-04-29 14:39:38 -0400

    Add Firebase dependencies. Set up Google sign in.

--- ios/Create Live/Create Live/ViewController.swift
+++ ios/Create Live/Create Live/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import FirebaseCrashlytics
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    let signInButton = GIDSignInButton()
+    signInButton.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+    view.addSubview(signInButton)
+    
+    GIDSignIn.sharedInstance()?.presentingViewController = self
   }
 
 
