651bec7604721ddcffeab759c7a66c4d74fe3e30
Chris Tibbs <eggyeggbean@google.com>
2020-04-30 14:12:14 -0400

    Add AVPlayer view to show stream in JoinStreamViewController, add utility classes for livestreaming

---
+++ ios/Create Live/Create Live/LivestreamUtils/StreamingToMUXManager.swift
@@ -0,0 +1,15 @@
+//
+//  StreamingToMUXManager.swift
+//  Create Live
+//
+//  Created by Chris Tibbs on 4/30/20.
+//  Copyright © 2020 FB Live. All rights reserved.
+//
+
+import UIKit
+
+class StreamingToMUXManager: NSObject {
+  class func startStreaming(with streamKey: String) {
+    
+  }
+}
