3fbdb0461473c8fd142ada854efa0bbc64869b4d
Chris Tibbs <eggyeggbean@google.com>
2020-04-30 16:10:38 -0400

    Add utility method for streaming to MUX via Streamaxia

--- ios/Create Live/Create Live/LivestreamUtils/StreamingToMUXManager.swift
+++ ios/Create Live/Create Live/LivestreamUtils/StreamingToMUXManager.swift
@@ -7,9 +7,40 @@
 //
 
 import UIKit
+import StreamaxiaSDK
 
-class StreamingToMUXManager: NSObject {
-  class func startStreaming(with streamKey: String) {
+/// Starts streaming to the MUX stream with the given streamkey. Shows the preview in the given view. Returns recorder object in a
+/// completion handler.
+class StreamingToMUXManager {
+  class func startStreaming(with streamKey: String,
+                            view: UIView,
+                            completion:@escaping (AXRecorder?, AXError?) -> Void) {
+    let info = AXStreamInfo.init()
     
+    info.useSecureConnection = false
+    info.customStreamURLString = "rtmp://global-live.mux.com:5222/app/\(streamKey)"
+    
+    let recordSettings = AXRecorderSettings.init()
+    
+    if let recorder = AXRecorder.init(streamInfo: info, settings: recordSettings) {
+      recorder.setup(with: view)
+      recorder.prepareToRecord()
+      
+      var error: AXError?
+      
+      recorder.activateFeatureAdaptiveBitRateWithError(&error)
+      if error != nil {
+        print("LIVLIVLIV adaptive bitrate failed.")
+      }
+      
+      recorder.startStreaming { (success, error) in
+        if error != nil {
+          assertionFailure()
+          completion(nil, error)
+        }
+        completion(recorder, nil)
+      }
+    }
+    completion(nil, AXError())
   }
 }
