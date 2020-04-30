6e6d9df8a1f175404d5f7a37420b020286347303
Tony <tony.huang.jun@gmail.com>
2020-04-29 19:04:25 -0700

    scaffolding

---
+++ android/app/src/main/java/com/tonyjhuang/fblive/ui/discovery/DiscoveryFragment.kt
@@ -0,0 +1,28 @@
+package com.tonyjhuang.fblive.ui.discovery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+
+import com.tonyjhuang.fblive.R
+
+class DiscoveryFragment : Fragment() {
+
+    private val viewModel: DiscoveryViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_discovery, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ android/app/src/main/java/com/tonyjhuang/fblive/ui/discovery/DiscoveryViewModel.kt
@@ -0,0 +1,7 @@
+package com.tonyjhuang.fblive.ui.discovery
+
+import androidx.lifecycle.ViewModel
+
+class DiscoveryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
