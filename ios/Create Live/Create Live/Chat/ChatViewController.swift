902bb16021e7d33fb67e44474f9ea01e1e7465e1
Chris Tibbs <eggyeggbean@google.com>
2020-05-01 11:38:27 -0400

    Add chat view controller that will be hosted in join stream controller

---
+++ ios/Create Live/Create Live/Chat/ChatViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ChatViewController.swift
+//  Create Live
+//
+//  Created by Chris Tibbs on 5/1/20.
+//  Copyright © 2020 FB Live. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBAction func sendMessageButtonTapped(_ sender: Any) {
+    
+  }
+  
+  let colors: [UIColor] = [
+    UIColor(red: 252.0/255.0, green: 100.0/255.0, blue: 113.0/255.0, alpha: 1.0),
+    UIColor(red: 98.0/255.0, green: 98.0/255.0, blue: 98.0/255.0, alpha: 1.0),
+    UIColor(red: 47.0/255.0, green: 186.0/255.0, blue: 160.0/255.0, alpha: 1.0),
+  ]
+  
+  var chat = DataProvider.aChat()
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    chat.messages.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let message = chat.messages[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: "message", for: indexPath)
+    let color = colors[indexPath.row % 3]
+    cell.textLabel?.attributedText = ChatViewController.attributedString(with: color, for: message)
+    return cell
+  }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+  }
+  
+  class func attributedString(with color: UIColor, for message: Message) -> NSAttributedString {
+    let usernameAttributes: [NSAttributedString.Key: Any] = [
+      .foregroundColor: color,
+      .font: UIFont.boldSystemFont(ofSize: 16)
+    ]
+    let userNameString = "\(message.user.name): "
+    let userNameAttrString = NSMutableAttributedString(string: userNameString,
+                                                       attributes: usernameAttributes)
+    
+    let messageAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.boldSystemFont(ofSize: 16)]
+    let messageAttrString = NSAttributedString(string: message.body, attributes: messageAttributes)
+    
+    userNameAttrString.append(messageAttrString)
+    
+    return userNameAttrString
+  }
+
+}
